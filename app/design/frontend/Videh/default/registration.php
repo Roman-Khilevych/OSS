f502480466b4c2c696a2f1c538525fb5a589ef34
Viktor Dehtiarov <videh@smile.fr>
2020-01-24 18:33:20 +0200

    create videh them

---
+++ app/design/frontend/Videh/default/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Videh theme
+ * @author Victor Dehtiyarov videh@smile.fr
+ * @copyright 2020 smile
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Videh/default', __DIR__);
