fc8c66e7b2c97c5cd694579d97f1d3b597e66421
zevsi <vgritsukm@gmail.com>
2020-01-28 17:16:13 +0200

    added new theme

---
+++ app/design/frontend/HrytsukWM/newTopic/registration.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ *Hrytsuk theme one
+ *@author Hrytsuk  vgritsukm@gmail.com
+ *@copyright 2020 zevsi
+ * */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/HrytsukWM/newTopic', __DIR__);
