cb29b5328f376f4abdf1a2496f667093d47284ac
vlromaniuk <vlad96r@protonmail.com>
2020-01-30 23:05:20 +0200

    homeWork Temaplte

--- app/design/frontend/template/default/registration.php
+++ app/design/frontend/template/default/registration.php
@@ -1,10 +1,9 @@
 <?php
 /**
-
 Vlromaniuk theme.
 @author Vladislav Romaniuk vlad96r@protonmial.com
 @copyright 2020 Vlromaniuk
  */
 use \Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/vlromaniuk/default', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/template/default', __DIR__);
