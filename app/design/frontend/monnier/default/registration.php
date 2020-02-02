a770dfe7936216c1c2cf506dadbe357f45782269
vlromaniuk <vlad96r@protonmail.com>
2020-02-02 12:42:31 +0200

    monnier init

---
+++ app/design/frontend/monnier/default/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+Vlromaniuk theme.
+@author Vladislav Romaniuk vlad96r@protonmial.com
+@copyright 2020 Vlromaniuk
+ */
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/monnier/default', __DIR__);
