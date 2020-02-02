80099b1670d077d56aae99417c0cf6037188381e
zevsi <vgritsukm@gmail.com>
2020-02-02 22:55:06 +0200

    home made Monnier

---
+++ app/design/frontend/Monnier/monnierTopic/registration.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ *Monnier nonnierTopic
+ *
+ *@author Hrytsuk  vgritsukm@gmail.com
+ *@copyright 2020 zevsi
+ * */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Monnier/monnierTopic', __DIR__);
