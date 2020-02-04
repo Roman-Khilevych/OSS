8e0236401dd0de8e3dc1e5d98fb9c264170e3d78
NikOleks <nikolaychuk.alexander@gmail.com>
2020-02-04 23:33:15 +0200

    cms class work

---
+++ app/design/frontend/lessonCmsBlock/default/registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * lesson CMS block theme
+ * @author Nikolaichuk Oleksandr nikolaychuk.alexander@gmail.com
+ * @copyright 2020 Nikolaichuk
+ */
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/lessonCmsBlock/default', __DIR__);
