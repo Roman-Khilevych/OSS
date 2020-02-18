77bb2c892663862cd99d22097eaccee4be2fc47d
zevsi <vgritsukm@gmail.com>
2020-02-18 17:48:50 +0200

    home rewrote css for less

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,36 @@
+
+'use strict';
+
+/**
+ * Define Themes
+ *
+ * area: frontend,
+ * name: Monniers/monnierTopic,
+ * locale: en_US,
+ * files: [
+ *'css/styles-m',
+ *'css/styles-l',
+ *'css/email',
+ *'css/email-inline'
+ * ],
+ * dsl: dynamic stylesheet language (less|sass)
+ *
+ */
+
+
+
+module.exports = {
+    monnier: {
+        area: 'frontend',
+        name: 'Monniers/monnierTopic',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    },
+
+};
