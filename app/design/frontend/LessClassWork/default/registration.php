00574c3ee79a25a3176fad6285128008b29fb780
YuriChlek <yurichlek@gmail.com>
2020-02-18 23:04:11 +0200

    Home work less Editorial

---
+++ app/design/frontend/LessClassWork/default/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *Chlek default theme registration
+ * @author Yuri Chlek yurichhlek@gmail.com
+ * @copyright 2020 Chlek
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/LessClassWork/default', __DIR__);
---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,41 @@
+/**
+ * Define Themes
+ *
+ * area: frontend,
+ * name: Chlek/default,
+ * locale: en_US,
+ * files: [
+ * 'css/styles-m',
+ * 'css/styles-l',
+ * 'css/email',
+ * 'css/email-inline'
+ * ],
+ * dsl: dynamic stylesheet language (less|sass)
+ *
+ */
+module.exports = {
+    LessClassWork: {
+        area: 'frontend',
+        name: 'LessClassWork/default',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    },
+    Editorial: {
+        area: 'frontend',
+        name: 'Editorial/default',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    }
+};
