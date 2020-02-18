2290261bf0bc577b6ca462ebba926c5cfca3f9f2
Viktor Dehtiarov <videh@smile.fr>
2020-02-18 19:00:38 +0200

    less

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Define Themes
+ *
+ * area: frontend,
+ * name: Newthem/default,
+ * locale: en_US,
+ * files: [
+ * 'css/styles-m',
+ * 'css/styles-l'
+ * 'css/email',
+ * 'css/email-inline'
+ * ],
+ * dsl: less
+ *
+ */
+module.exports = {
+    mythem: {
+        area: 'frontend',
+        name: 'Newthem/default',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    }
+};
