6c869cd0d5c932e333e1d38c3633dd6231d39765
vlromaniuk <vlad96r@protonmail.com>
2020-02-23 12:52:17 +0200

    monnier css to less

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,14 @@
+module.exports = {
+    monier: {
+        area: 'frontend',
+        name: 'monnier/default',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    }
+};
