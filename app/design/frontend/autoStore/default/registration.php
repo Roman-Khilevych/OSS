bf06f993f02bc34a697c627f9948440cf897d5ee
NikOleks <nikolaychuk.alexander@gmail.com>
2020-03-10 02:53:36 +0200

    Auto Store category page

---
+++ app/design/frontend/autoStore/default/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * AutoStore default theme registration
+ *
+ * @author Nikolaichuk Oleksandr nikolaychuk.alexander@gmail.com
+ * @copyright 2020 Nikolaichuk
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/autoStore/default', __DIR__);
