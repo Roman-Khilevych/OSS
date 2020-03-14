ec7b4c8b16f9fa6dd2564393089035da731d3e2b
YuriChlek <yurichlek@gmail.com>
2020-03-14 15:43:00 +0200

    Fixed errors

---
+++ app/code/AutoStoreKo/Sample/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'AutoStore_Sample', __DIR__);
--- app/design/frontend/AutoStore/default/Magento_Catalog/web/js/owl-include.js
+++ app/design/frontend/AutoStore/default/Magento_Catalog/web/js/owl-include.js
@@ -6,32 +6,30 @@ define([
         'use strict';
 
         return function () {
-            $(document).ready(function () {
-                $('.owl-carousel').owlCarousel({
-                    loop: true,
-                    margin: 0,
-                    nav: true,
-                    responsiveClass: true,
-                    responsive: {
-                        0: {
-                            items: 1,
-                            nav: false
-                        },
-                        480: {
-                            items: 2,
-                            nav: false
-                        },
-                        768: {
-                            items: 3,
-                            nav: false
-                        },
-                        1024: {
-                            items: 4,
-                            nav: true,
-                            loop: false
-                        }
+            $('.owl-carousel').owlCarousel({
+                loop: true,
+                margin: 0,
+                nav: true,
+                responsiveClass: true,
+                responsive: {
+                    0: {
+                        items: 1,
+                        nav: false
+                    },
+                    480: {
+                        items: 2,
+                        nav: false
+                    },
+                    768: {
+                        items: 3,
+                        nav: false
+                    },
+                    1024: {
+                        items: 4,
+                        nav: true,
+                        loop: false
                     }
-                })
+                }
             })
         }
     });
